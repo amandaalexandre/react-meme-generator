d15dc3042b45c6d21f788d55be725d03f56b3283
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-07-27 17:27:16 -0300

    feat: switch images

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Data from '../memesData'
 import '../assets/styles/main.css'
-import meme from '../assets/images/memeimg.png'
 
 export default function Main() {
-    
+    const [allMemeImages, setAllMemeImages] = useState(Data.data.memes)
+
+    // colocando a primeira imagem pra ser a primeira do BD
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    });
+
+    const getMemeImage = () => {
+        // finds a random index
+        const randomIndex = Math.floor(Math.random() * Data.data.memes.length);
+
+        // assigns it to variable memeUrl
+        const memeUrl = Data.data.memes[randomIndex].url;
+        console.log(memeUrl)
+
+        setMeme((prevMeme) => {
+            return {...prevMeme, randomImage: memeUrl}
+        })
+        console.log(meme.randomImage)
+    }
 
     return (
         <main>
-            <input type="text" />
-            <input type="text" />
-            <button type="submit">Get a new meme image 🖼️</button>
-            <div className='meme-generated'>
-                <img src={meme}/>
+            <div className='form'>
+                <input type="text" />
+                <input type="text" />
+                <button onClick={getMemeImage}>Get a new meme image 🖼️</button>
+                <div id='meme-generated'>
+                    <img src={meme.randomImage} className='meme-image' />
+                </div>
             </div>
-            
         </main>
     )
 }
